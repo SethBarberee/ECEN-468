5ca929219fb0b6802298dd7990766a9845639306
Seth Barberee <seth.barberee@gmail.com>
2020-02-22 07:59:22 -0600

    Lab 6 started

---
+++ Lab6/test_RAM.cpp
@@ -0,0 +1,119 @@
+#include "systemc.h"
+#include "RAM.cpp"
+#include <scv.h>
+
+class Pkt_constraint : virtual public scv_constraint_base
+{
+	public:
+	scv_smart_ptr<sc_uint<ADDR_WIDTH> > sAddr;
+	scv_smart_ptr<sc_uint<DATA_WIDTH> > sInData;
+	scv_smart_ptr<sc_uint<DATA_WIDTH> > sOutData;
+
+	SCV_CONSTRAINT_CTOR(Pkt_constraint) {
+		// define constraints
+                SCV_SOFT_CONSTRAINT( sAddr() < 10);
+                SCV_SOFT_CONSTRAINT( sInData() > 80);
+	}
+};
+
+int sc_main (int argc, char* argv[]) {
+
+	// Declare Input/Output Signals
+  	sc_signal < sc_uint<ADDR_WIDTH> > 	tAddr ;
+  	sc_signal < bool > 			tbWE;
+  	sc_signal < bool > 			tbCE;
+  	sc_signal < sc_uint<DATA_WIDTH> > 	tInData;
+  	sc_signal < sc_uint<DATA_WIDTH> > 	tOutData;
+  	
+	int i = 0;
+	
+	// Connect the DUT(Design Under Test)
+	RAM RAM_01("SIMULATION_RAM");
+		RAM_01.InData(tInData);
+        	RAM_01.Addr(tAddr);
+		RAM_01.bCE(tbCE);
+		RAM_01.bWE(tbWE);
+		RAM_01.OutData(tOutData);
+
+	// Open VCD(Value Change Dump) file
+	sc_trace_file *wf = sc_create_vcd_trace_file("VCD_RAM");
+
+	// Dump the desired signals
+	sc_trace(wf, tInData, "strInData");
+        sc_trace(wf, tAddr, "strAddr");
+	sc_trace(wf, tbCE, "strbCE");
+	sc_trace(wf, tbWE, "strbWE");
+	sc_trace(wf, tOutData, "strOutData");
+
+	// Initialize all variables
+	tbCE.write(1);
+	tbWE.write(1);
+	sc_start(5);
+
+	Pkt_constraint cPkt("Pkt_Test");	
+
+        // Verification I
+        cout << "------------------- Beginning of Verification I" << endl;
+        for(i=0; i<5; i++){
+          // Set writing mode
+	  // ...
+            tBCE.write(0);
+            tBWE.write(0);
+          
+          // Generate values of tInData & tAddr using "cPkt"
+	  // ...
+            cPkt->next();
+            tInData.write(cPkt.sInData);
+            tAddr.write(cPkt.sAddr)
+          // Set reading mode
+	  // ...
+            tBCE.write(0);
+            tBWE.write(1);
+          // Data read from tOutData to sOutData
+            cPkt.sOutData = tOutData.read();
+          // Print statistics
+	  // ...
+            cout << "Statistics" << endl;
+            cout << "InData: " << cPkt.sInData << endl; 
+            cout << "Addr: " << cPkt.sAddr << endl; 
+            cout << "OutData: " << cPkt.sOutData << endl; 
+        }
+        cout << "------------------- End of Verification I" << endl << endl;
+        
+        // Verification II
+        cout << "------------------- Beginning of Verification II" << endl;        
+        for(i=0; i<5; i++){
+          // Set writing mode
+	  // ...
+            tBCE.write(0);
+            tBWE.write(0);
+
+          // Set range distribution
+          // InData with 5% from 80 to 99 and 95% from 100 to 120
+          scv_bag<sc_uint<DATA_WIDTH>> intBag;
+          
+	  // Generate values of tInData  using "cPkt"
+	  // ...
+          cPkt.sInData->next();
+          tInData.write(cPkt.sInData);
+          tAddr.write(cPkt.sAddr)          
+          // Set reading mode
+          tBCE.write(0);
+          tBWE.write(1);
+
+          // Data read
+            sOutData = tOutData.read();
+          // Print statistics
+	  // ...
+            cout << "Statistics" << endl;
+            cout << "InData: " << cPkt.sInData << endl; 
+            cout << "Addr: " << cPkt.sAddr << endl; 
+            cout << "OutData: " << cPkt.sOutData << endl; 
+        }
+        cout << "------------------- End of Verification II" << endl;
+        
+	// Close trace file
+	sc_close_vcd_trace_file(wf);
+	
+	return 0;	// Terminate simulation
+}
