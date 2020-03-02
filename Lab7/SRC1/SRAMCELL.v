ff5223433fd97e38f0cd58f3aec5c5ea37350988
Seth Barberee <seth.barberee@gmail.com>
2020-03-02 06:07:47 -0600

    hopefully trucked through all the code for Lab7

--- Lab7/SRC1/SRAMCELL.v
+++ Lab7/SRC1/SRAMCELL.v
@@ -1,27 +1,22 @@
 module SRAMCELL (InData, OutData, bCS, bWE);
 
 // Declare input/output signals
-// ...
 input InData;
 output OutData;
 input bCS;
 input bWE;
 
 // Internal register/wire
-// ...
-wire data_in;
-wire cs_we;
 reg d;
 
-and(data_in, InData, !bWE);
-and(cs_we, bWE, d);
-or(d, data_in, cs_we);
 
 
 // Functional Module Description
-always @(d or bCS)	// Sensitivity
+always @(InData or bCS)	// Sensitivity
 begin
-        if(!bCS)
+        if (!bCS && !bWE)
+            d <= InData;
+        else if(!bCS)
             OutData <= d;
 end
 endmodule
--- Lab7/SRC2/SRAM.v
+++ Lab7/SRC2/SRAM.v
@@ -4,22 +4,29 @@ parameter AddressSize = 18;		// 2^18 = 256K
 parameter WordSize = 8;			// 8 bits
 
 // Port Declaration
-// ...
-// ...
+input [AddressSize - 1: 0] Address;
+input [WordSize - 1: 0] InData;
+input bCE;
+input bWE;
+output [WordSize - 1: 0] OutData;
 
 // Internal Variable
-// ...
+reg [WordSize - 1: 0] internal_memory [AddressSize << 1];
 
 // Function Read
 always @(bCE or bWE or Address)
 begin
-	// ...
+	if(bWE && !bCE)
+            // read from storage
+            OutData <= internal_memory[Address];
 end
 
 // Function Write
 always @(bCE or bWE or Address or InData)
 begin
-	// ...
+	if(!bWE && !bCE)
+            // write to storage
+            internal_memory[Address] <= InData;
 end
 
 endmodule
