666ce0b950c54eebd0a85c8ac9cc30acafd244b2
Seth Barberee <seth.barberee@gmail.com>
2020-03-21 09:42:12 -0500

    add files for Lab 9

---
+++ Lab9/SRC/SRAM.v
@@ -0,0 +1,34 @@
+module SRAM (Address, InData, OutData, bCE, bWE);
+
+parameter AddressSize = 18;		// 2^18 = 256K
+parameter WordSize = 8;			// 8 bits
+
+// Port Declaration
+input [AddressSize - 1: 0] Address;
+input [WordSize - 1: 0] InData;
+output [WordSize - 1: 0] OutData;
+input bCE;
+input bWE;
+
+// Internal Variable
+reg [WordSize - 1: 0] internal_memory [(1 << AddressSize) - 1 : 0];
+reg [WordSize - 1: 0] OutData;
+
+// Function Read
+always @(bCE or bWE or Address)
+begin
+	if(bWE && !bCE)
+        // read from storage
+        OutData <= internal_memory[Address];
+end
+
+// Function Write
+always @(bCE or bWE or Address or InData)
+begin
+	if(!bWE && !bCE)
+        // write to storage
+        internal_memory[Address] <= InData;
+end
+
+endmodule
+
